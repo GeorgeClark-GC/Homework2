a217241253d7db6688b14166a174f9b5399649bf
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-06-23 18:07:26 +0300

    HW2_4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog - 3.5;
-        System.out.println(dog);
-        cat = cat - 1.6;
-        System.out.println(cat);
-        paper = paper - 7639;
-        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 3;
+        System.out.println(friend);
     }
 }
